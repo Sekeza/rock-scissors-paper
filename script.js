00a3a34bd6e06698f98961bb9f84beeb01ff168f
Seyi Samuel <Seyi.Samuel10@yahoo.com>
2022-08-08 00:05:06 +0100

    Add the game function, and apply conditional statement in it

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const choiceArray = ["Rock", "Paper", "Scissors"];
+const choiceArray = ["Rock", "Scissors", "Paper"];
+
 
 const getComputerChoice = () => {
   const randomNumber = Math.floor(Math.random() * choiceArray.length);
@@ -6,12 +7,16 @@ const getComputerChoice = () => {
   return cpuChoice;
 };
 
+let computerSelection;
+let userInput;
+
 const playGameRound = (playerSelection, computerSelection) => {
-    let userPick = playerSelection.toLowerCase();
-    let cpuPick = computerSelection.toLowerCase();
+    let userPick = playerSelection;
+    let cpuPick = computerSelection;
 
     console.log("person: " + userPick); 
     console.log( "computer: " + cpuPick);
+
     if((userPick + cpuPick) === "rockscissors" || (userPick + cpuPick) === "paperrock" || (userPick + cpuPick) === "scissorspaper" ) {
         return "You win!"
     }
@@ -26,7 +31,33 @@ const playGameRound = (playerSelection, computerSelection) => {
     }
 };
 
-playGameRoundD();
+
+const game = () => {
+    let userScore = 0; 
+    let cpuScore = 0;
+    for(let i = 0; i < 5; i++) {
+        userInput = prompt("Enter either 'Rock', 'Paper', 'Scissors' to play: ").toLowerCase();
+        computerSelection = getComputerChoice().toLowerCase();
+        
+        console.log("userInput: " + userInput);
+        console.log("computerSelection: " + computerSelection);
+
+        let result = playGameRound(userInput, computerSelection);
+        if(result === "You win!") {
+             userScore++;
+        }
+        if(result === "You lost!") {
+             cpuScore++;  
+        }
+        console.log("userScore: " + userScore);
+        console.log("cpuScore: " + cpuScore);
+        console.log(result);        
+    }
+    return userScore > cpuScore ? "Player one wins" : userScore === cpuScore ? "It's a deuce-Draw" : "Computer wins";
+}
+
+game();
+
 
 
 
