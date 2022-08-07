1ee8ca6057cd2664cd6f1d36631393ccfb1d7652
Seyi Samuel <Seyi.Samuel10@yahoo.com>
2022-08-07 22:27:32 +0100

    Added the playGameRound function to decides the winner or loser or tie in the game

--- script.js
+++ script.js
@@ -6,5 +6,25 @@ const getComputerChoice = () => {
   return cpuChoice;
 };
 
+const playGameRound = (playerSelection, computerSelection) => {
+    let userPick = playerSelection.toLowerCase();
+    let cpuPick = computerSelection.toLowerCase();
+
+    console.log("person: " + userPick); 
+    console.log( "computer: " + cpuPick);
+    if((userPick + cpuPick) === "rockscissors" || (userPick + cpuPick) === "paperrock" || (userPick + cpuPick) === "scissorspaper" ) {
+        return "You win!"
+    }
+    else if((userPick + cpuPick) === "scissorsrock" || (userPick + cpuPick) === "rockpaper" || (userPick + cpuPick) === "paperscissors" ) {
+        return "You lost!"
+    }
+    else if((userPick + cpuPick) === "rockrock" || (userPick + cpuPick) === "scissorsscissors" || (userPick + cpuPick) === "paperpaper" ) {
+        return "It's a draw!"
+    }
+    else {
+        return "Dont get there!"
+    }
+};
+
 
 
